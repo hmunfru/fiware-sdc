dc7e8e1345180952cf8c92ab7675811f286125fa
Jesus Perez <jesuspg@tid.es>
2015-04-30 12:13:28 +0200

    refactor: remove euro_iaas.common.properties dependency

--- core/src/main/java/com/telefonica/euro_iaas/sdc/bootstrap/PropertiesMBeanRegisterBootstrap.java
+++ core/src/main/java/com/telefonica/euro_iaas/sdc/bootstrap/PropertiesMBeanRegisterBootstrap.java
@@ -33,11 +33,11 @@ import javax.servlet.ServletContextListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import com.telefonica.euro_iaas.commons.properties.PropertiesProvider;
-import com.telefonica.euro_iaas.commons.properties.PropertiesProviderMBean;
-import com.telefonica.euro_iaas.commons.properties.impl.PropertiesProviderFactoryImpl;
-import com.telefonica.euro_iaas.commons.properties.mbeans.MBeanUtils;
 import com.telefonica.euro_iaas.sdc.exception.SdcRuntimeException;
+import com.telefonica.fiware.commons.properties.PropertiesProvider;
+import com.telefonica.fiware.commons.properties.impl.PropertiesProviderFactoryImpl;
+import com.telefonica.fiware.commons.properties.mbeans.MBeanUtils;
+import com.telefonica.fiware.commons.properties.mbeans.PropertiesProviderMBean;
 
 /**
  * Load System Properties file and stores it in BD (not modify already stored properties).
