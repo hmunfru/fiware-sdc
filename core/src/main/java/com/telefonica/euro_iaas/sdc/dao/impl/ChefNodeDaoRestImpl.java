d897d6244daf4e04154c93d9023d2d82af7fc243
Jesus M Movilla <jesus.movilla@altran.com>
2015-05-05 14:16:19 +0200

    Replacing MAX_TIME by REGISTRATION_MAXTIME in chefInstallator

--- core/src/main/java/com/telefonica/euro_iaas/sdc/dao/impl/ChefNodeDaoRestImpl.java
+++ core/src/main/java/com/telefonica/euro_iaas/sdc/dao/impl/ChefNodeDaoRestImpl.java
@@ -72,8 +72,7 @@ public class ChefNodeDaoRestImpl implements ChefNodeDao {
 
     private String NODE_NOT_FOUND_PATTERN = "404";
     private String NODES_PATH = "/nodes";
-    private static final int MAX_TIME = 900000;
-
+   
     public ChefNode loadNodeFromHostname(String hostname, String token) throws EntityNotFoundException,
             CanNotCallChefException {
         log.info("Loading nodes " + hostname);
@@ -256,7 +255,7 @@ public class ChefNodeDaoRestImpl implements ChefNodeDao {
         while (!response.contains(hostname)) {
 
             try {
-                log.info("Checking node : " + hostname + " MAX_TIME: " + MAX_TIME + " time:" + time);
+                log.info("Checking node : " + hostname + " REGISTRATION_MAXTIME: " + REGISTRATION_MAXTIME + " time:" + time);
                 if (time > REGISTRATION_MAXTIME) {
                     String errorMesg = "Node  " + hostname + " is not registered in ChefServer";
                     log.info(errorMesg);
